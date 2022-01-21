d14f2245feff25c09709286be4c68723dbaf6bfb
A2U8C <69053051+A2U8C@users.noreply.github.com>
2022-01-21 04:21:10 -0800

    Commit

--- website/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-
-
-app = Flask(__name__)
-app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
-
-app.config["IMAGE_UPLOADS"] = "website/static/Image_Uploads"
-from website import routes
